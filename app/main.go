34af153012d8599f6ad0ad566fd46cffed625014
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-10-09 23:17:42 -0400

    Progress in interpretting the record bytes

--- app/main.go
+++ app/main.go
@@ -10,11 +10,56 @@ import (
 	// "github.com/xwb1989/sqlparser"
 )
 
+func decodeContentSize(serialType int64) {
+
+}
+
+
+func parseRecord(recordRaw []byte, headerSize int64) {
+	totalBytesCountHeader, hSize := binary.Varint(recordRaw);
+
+	var currOffset int64;
+	currOffset = int64(hSize);
+
+	colIndex := 0;
+
+	var colsSerialTypes []int64
+	var colContents [][]byte
+
+	for currOffset < totalBytesCountHeader {
+		serialType, bytesRead := binary.Varint(recordRaw);
+		colsSerialTypes = append(colsSerialTypes, serialType);
+	}
+
+	//!Start reading from headerSizeOffset. And keep on interpretting everything
+
+
+
+	A record contains a header and a body, in that order. 
+	The header begins with a single varint which determines the total number of bytes in the header. 
+	The varint value is the size of the header in bytes including the size varint itself. 
+	Following the size varint are one or more additional varints, one per column. 
+	These additional varints are called "serial type" numbers and determine the datatype of each column, according to the following chart:
+
+
+
+}
+
+
 func parsePage(rawBytes []byte, pageSize int, firstPage bool) {
+	//!Since cell pointers are from beginning of the page, it is better to send whole page.
+
+
 	//!If it is the first page, page size will be 100 bytes less which has removed the header.
+	var pageHeaderIndex int
+	if(firstPage) {
+		pageHeaderIndex = 100;
+	} else {
+		pageHeaderIndex = 0;
+	}
 
 	var pageHeaderSize int
-	if rawBytes[0] == 0x05 || rawBytes[0] == 0x02 {	//!Interior table page.
+	if rawBytes[pageHeaderIndex] == 0x05 || rawBytes[pageHeaderIndex] == 0x02 {	//!Interior table page.
 		pageHeaderSize = 12;
 	}	else {
 		pageHeaderSize = 8;
@@ -22,19 +67,48 @@ func parsePage(rawBytes []byte, pageSize int, firstPage bool) {
 
 	//!Next step is to get the cell count on the page.
 	var cellCount uint16;
-	cellCountBytes := bytes.NewReader(rawBytes[3:5]);
+	cellCountBytes := bytes.NewReader(rawBytes[pageHeaderIndex + 3: pageHeaderIndex + 5]);
 	binary.Read(cellCountBytes, binary.BigEndian, &cellCount);
 
 	k = int(cellCount);
 
 	cellPointers := make([]uint16, k);
 
+	cellPointerArrayOffset := pageHeaderIndex + pageHeaderSize;
+
 	//!Now read cell pointer array and store them.
 	for i := 0; i < k; i++ {
-		currentPointerBytesReader := bytes.NewReader(rawBytes[pageHeaderSize + (k *2) : (k + 1)* 2]);
+		currentPointerBytesReader := bytes.NewReader(rawBytes[cellPointerArrayOffset + (k *2) : cellPointerArrayOffset + (k + 1)* 2]);
 		binary.Read(currentPointerBytesReader, binary.BigEndian, &(cellPointers[i]));
     }
+
+	for i := 0; i <k; i++ {
+		currCellPointer := cellPointers[i];
+		//!total bytes of playload
+		//!Since max size of varint would be 9 bytes, we can pass only 9 bytes for interpretation.
+
+		payloadSize, sizeBytesRead := binary.Varint(rawBytes[currCellPointer : currCellPointer + 9]);
+		_, rowIdBytesRead := binary.Varint(rawBytes[currCellPointer +uint16(sizeBytesRead) : currCellPointer + uint16(sizeBytesRead) + 9]);
+
+		//!byte array of payload
+		//!This will be the record format:		
+		currCellPayload := rawBytes[uint64(currCellPointer) +uint64(sizeBytesRead)+uint64(rowIdBytesRead) : uint64(currCellPointer)+uint64(sizeBytesRead)+uint64(rowIdBytesRead)+uint64(payloadSize)]
+// fmt.Printf("payload: %v\n", string(record))
+
+
+
+	}
+
+	//!There cell pointers are basically offset from top of the page. From 0
 	
+	//!Note:
+	//!Also harding coding it to be Table B-Tree Leaf Cell 
+	//!Also, no overflow will happen, so no need to worry about it.
+
+
+
+
+
 	//!At this point we have gotten pointer to every cell on this page. --> Next step would be to go to these cells and get the name
 
 
