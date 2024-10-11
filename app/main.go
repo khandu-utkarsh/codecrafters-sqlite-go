ba6263f7d6f19f195347ea6eec5f84e3e44d528b
utkarsh <utkarshkhandelwal2011@gmail.com>
2024-10-10 22:37:50 -0400

    All the pieces are almost ready, except one part

--- app/main.go
+++ app/main.go
@@ -194,68 +194,43 @@ func main() {
 
 	case ".tables":
 
-		//!Return the name of tables present in the db file.
+		databaseFile, err := os.Open(databaseFilePath)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		//!Steps:
-		//!Open the DB file. Then go to the page header section.
+		header := make([]byte, 100)
+		_, err = databaseFile.Read(header)
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		var pageSize uint16
+		if err := binary.Read(bytes.NewReader(header[16:18]), binary.BigEndian, &pageSize); err != nil {
+			fmt.Println("Failed to read integer:", err)
+			return
+		}
 
-		// databaseFilePath := os.Args[1]
-		// command := os.Args[2]
-	
-		// switch command {
-		// case ".dbinfo":
-		// 	databaseFile, err := os.Open(databaseFilePath)
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-	
-		// 	header := make([]byte, 100)
-	
-		// 	_, err = databaseFile.Read(header)
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-	
-		// 	var pageSize uint16
-		// 	if err := binary.Read(bytes.NewReader(header[16:18]), binary.BigEndian, &pageSize); err != nil {
-		// 		fmt.Println("Failed to read integer:", err)
-		// 		return
-		// 	}
-		// 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-		// 	//fmt.Println("Logs from your program will appear here!")
-	
-		// 	// Uncomment this to pass the first stage
-		// 	fmt.Printf("database page size: %v\n", pageSize)
-	
-		// 	//!By default it is page 1 is at offset zero and contains sqlite_schema.
-		// 	//!var default offset with the size 100
-	
-		// 	pageHeader := make([]byte, 12)
-		// 	_, err = databaseFile.Read(pageHeader)
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-			
-		// 	var cellsCount uint16;
-		// 	if err := binary.Read(bytes.NewReader(pageHeader[3:5]), binary.BigEndian, &cellsCount); err != nil {
-		// 		fmt.Println("Failed to get cell count:", err)
-		// 		return
-		// 	}
-	
-		// 	intCellCount := int(cellsCount);
-		// 	// Logging the cell count, which is same as tables count in this case, since we don't have other things like index, views, triggers etc.
-		// 	fmt.Printf("number of tables: %v", intCellCount)
-	
-	
-		// 	databaseFile.Close();
-	
+		pageBytes := make([]byte, int64(pageSize));
+		databaseFile.ReadAt(pageBytes, 0);
+
+		//!First page will have sql_schema table and need to go through each row having info of the table and extract table name from it.
+		cellPointers := getCellPointersOnPage(pageBytes, int64(pageSize), true);
+
+		var names string[];
+
+		// Iterate using range
+		for _, cellPointer := range cellPointers {
+			tableRowContent := readCell(pageBytes, cellPointer);
+			names = append(names, tableRowContent.name);
+		}
+
+		for _, name := range names {
+			fmt.Println(name);
+		}
 
 
-
-		//!Read the page 1 --> Because sql lite schema will be on that page.
-		//!Go to cell pointer array to get the offset of each cell
-		//!Go to each cell and there go to payload place. 
+		databaseFile.Close();
 
 
 
